5423145ab9b9146ddfb87f279839c3e806118485
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2026-05-28 22:31:08 +0530

    Update vite.config.ts

--- vite-host/vite.config.ts
+++ vite-host/vite.config.ts
@@ -5,7 +5,7 @@ import { federation } from '@module-federation/vite';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
-  const base = isProd ? `${process.env.GH_PAGES_BASE_URL}/vite-host/` : "/";
+  const base = isProd ? `${process.env.GH_PAGES_BASE_URL ?? ""}/vite-host/` : "/";
   return {
     base,
     plugins: [react(),
@@ -16,19 +16,19 @@ export default defineConfig(({ mode }) => {
         app1: {
           // type: "module",
           name: "app1",
-          entry: isProd ? "/app1/mf-manifest.json" : "http://localhost:3001/mf-manifest.json",
+          entry: isProd ? `${base}/app1/mf-manifest.json` : "http://localhost:3001/mf-manifest.json",
         },
         app2: {
           name: "app2",
-          entry: isProd ? "/app2/mf-manifest.json" : "http://localhost:3002/mf-manifest.json",
+          entry: isProd ? `${base}/app2/mf-manifest.json` : "http://localhost:3002/mf-manifest.json",
         },
         app3: {
           name: "app3",
-          entry: isProd ? "/app3/mf-manifest.json" : "http://localhost:3003/mf-manifest.json",
+          entry: isProd ? `${base}/app3/mf-manifest.json` : "http://localhost:3003/mf-manifest.json",
         },
         app4: {
           name: "app4",
-          entry: isProd ? "/app4/mf-manifest.json" : "http://localhost:3004/mf-manifest.json",
+          entry: isProd ? `${base}/app4/mf-manifest.json` : "http://localhost:3004/mf-manifest.json",
         },
       },
       shareStrategy: "loaded-first",
